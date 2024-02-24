b554d226a1116ea087de9dcd6fa097681b313f29
rupashreesmagic <rupashreesmagic@gmail.com>
2024-02-24 22:17:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ClockHeading from "./Components/ClockHeading";
+import ClockSlogan from "./Components/ClockSlogan";
+import CurrentTime from "./Components/CurrentTime"; 
+function App(){
+   return <center>
+        <ClockHeading></ClockHeading>
+        <ClockSlogan></ClockSlogan>
+        <CurrentTime></CurrentTime>
+   </center>
+        
+}
+export default App;
---
+++ src/Components/ClockSlogan.jsx
@@ -0,0 +1,4 @@
+let ClockSlogan = () => {
+    return <p className = "lead">This is the clock that shows the time in Bharat at all times.</p>
+}
+export default ClockSlogan;
